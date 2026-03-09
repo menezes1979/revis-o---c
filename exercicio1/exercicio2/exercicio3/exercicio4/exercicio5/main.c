0ed47ab13811a12053de6823c77ca6605c87da88
menezes1979 <juliocezarmenezesdossantos5@gmail.com>
2026-03-08 22:16:33 -0300

    exercicio5

---
+++ exercicio1/exercicio2/exercicio3/exercicio4/exercicio5/main.c
@@ -0,0 +1,23 @@
+exercicio5/main.c
+  /*
+a) Vetor é uma estrutura de dados que armazena vários valores
+do mesmo tipo em posições consecutivas de memória.
+
+Exemplo:
+int numeros[5];
+
+Cada posição pode ser acessada por índice:
+numeros[0], numeros[1], numeros[2]...
+
+b) String em C é um vetor de caracteres terminado
+pelo caractere especial '\0'.
+
+Exemplo:
+char nome[20];
+
+Ela armazena textos como palavras ou frases.
+*/
+
+int main(){
+return 0;
+}
