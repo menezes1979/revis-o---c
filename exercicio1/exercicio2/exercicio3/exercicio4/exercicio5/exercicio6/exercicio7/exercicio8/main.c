67654ab678f1a8502d36a526bf1d24b44af32a0e
menezes1979 <juliocezarmenezesdossantos5@gmail.com>
2026-03-08 22:20:55 -0300

    exercicio8

---
+++ exercicio1/exercicio2/exercicio3/exercicio4/exercicio5/exercicio6/exercicio7/exercicio8/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+a) free libera a memória que foi alocada dinamicamente.
+
+b) Não é recomendado usar o ponteiro após o free.
+
+c) Vazamento de memória ocorre quando a memória
+é alocada mas nunca liberada.
+
+d) malloc retorna NULL quando não consegue alocar memória.
+
+e) calloc aloca memória e inicializa todos os bytes com zero.
+
+f) Diferença:
+
+malloc -> não inicializa memória
+calloc -> inicializa memória com zero
+*/
+
+int main(){
+return 0;
+}
