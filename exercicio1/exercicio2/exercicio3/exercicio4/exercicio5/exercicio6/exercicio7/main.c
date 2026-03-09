bcd7d24dda4201b318c1f9bba04de276f74b5c39
menezes1979 <juliocezarmenezesdossantos5@gmail.com>
2026-03-08 22:19:17 -0300

    exercicio7

---
+++ exercicio1/exercicio2/exercicio3/exercicio4/exercicio5/exercicio6/exercicio7/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+a) Modos de abertura de arquivos:
+
+r  -> leitura
+w  -> escrita
+a  -> adicionar ao final
+rb -> leitura binária
+wb -> escrita binária
+
+b)
+
+fgets:
+Lê uma linha de texto de um arquivo.
+
+fprintf:
+Escreve dados formatados em um arquivo.
+
+fread:
+Lê dados binários de um arquivo.
+
+fwrite:
+Escreve dados binários em um arquivo.
+*/
+
+int main(){
+return 0;
+}
