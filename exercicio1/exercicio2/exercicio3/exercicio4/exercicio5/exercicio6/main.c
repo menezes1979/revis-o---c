26fe0e97876327deaab707ee5ffd0e8822d4187e
menezes1979 <juliocezarmenezesdossantos5@gmail.com>
2026-03-08 22:18:25 -0300

    exercicio6

---
+++ exercicio1/exercicio2/exercicio3/exercicio4/exercicio5/exercicio6/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+a) Saída do programa:
+
+while: 0
+while: 1
+while: 2
+while: 3
+while: 4
+while: 5
+while: 6
+while: 7
+while: 8
+while: 9
+do: 10
+
+b) Diferença:
+while testa a condição antes de executar.
+do while executa primeiro e depois testa.
+
+c) O bloco executa repetidamente enquanto
+a condição for verdadeira.
+
+d) while é usado quando não sabemos o número
+de repetições.
+
+for é usado quando sabemos quantas vezes
+o loop vai repetir.
+*/
+
+int main(){
+return 0;
+}
