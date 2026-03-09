47719b3888b1ae3bba08debf4353a1ea4a5bc7eb
menezes1979 <juliocezarmenezesdossantos5@gmail.com>
2026-03-08 22:23:27 -0300

    exercicio11

---
+++ exercicio1/exercicio2/exercicio3/exercicio4/exercicio5/exercicio6/exercicio7/exercicio8/exercicio9/exercicio10/exercicio11/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+a) Diferença:
+
+Pilha (Stack):
+último que entra é o primeiro que sai (LIFO).
+
+Fila (Queue):
+primeiro que entra é o primeiro que sai (FIFO).
+
+b) Vantagens da fila:
+organização e controle de ordem.
+
+Desvantagens:
+acesso apenas ao primeiro elemento.
+
+c) Vantagens da pilha:
+simples e rápida.
+
+Desvantagens:
+acesso apenas ao topo.
+
+d) Aplicações:
+
+Pilha:
+navegadores, desfazer ações.
+
+Fila:
+impressoras, atendimento, sistemas de espera.
+
+e) Diferença para lista:
+
+Lista encadeada permite inserir ou remover
+elementos em qualquer posição.
+*/
+
+int main(){
+return 0;
+}
